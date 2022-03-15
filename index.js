8c617534548357d198e28f34a9a04c766ac67970
unknown <clamy6@yahoo.fr>
2022-03-15 14:17:21 +0100

    restau routes

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 
 // ROUTES
 const hotelsRoutes = require("./routes/hotelsRoutes");
+const restaurantsRoutes = require("./routes/restaurantsRoutes");
 app.use(express.json());
 app.use("/hotels", hotelsRoutes);
-app.use("*", (err, req, res) => {
+app.use("/restaurants", restaurantsRoutes);
+app.use("*", (req, res) => {
   res.status(404).send("404 error");
 });
 
